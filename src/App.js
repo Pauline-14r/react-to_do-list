847111d218791ed40ae44102b5ef8aa923b52255
Poline <pauline14r@gmail.com>
2026-04-13 15:44:40 +0300

    feat: add input with layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
+import logo from "./pictures/logo.svg";
 
 function App() {
+	const [inputText, setText] = useState('');
+	const [tasks, setTasks] = useState([]);
+	
+	function onAdd() {
+		if (inputText.trim() === '') {
+			return alert('Пустое поле задач');
+		}
+		setTasks((prev) => {
+			return [...prev, inputText.trim()]
+		});
+		setText('')
+	}
+
+	function onKeyDown(e) {
+		if (e.key === 'Enter') {
+			return onAdd();
+		}
+	}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page-wrapper">
+			<Header />
+			<div className="main">
+				<div className="task-area">
+					<InputArea
+						inputText={inputText}
+						setText={setText}
+						onAdd={onAdd}
+						onKeyDown={onKeyDown} />
+					<ul>
+						{tasks.map((task, index) => <li key={index}>{task}</li>)}
+					</ul>
+				</div>
+			</div>
     </div>
   );
 }
+
+function InputArea({inputText, setText, onAdd, onKeyDown}) {
+	return (
+		<div className="input-area">
+		<input
+			className="input-area_input" 
+			value = {inputText}
+			onChange={(e) => setText(e.target.value)}
+			onKeyDown={onKeyDown}
+			placeholder='Добавить новую задачу'></input>
+			<button 
+			className="input-area_button"
+			onClick={onAdd}
+			>Добавить</button>
+		</div>
+	)
+}
+
+function Header() {
+	return (
+		<div className="header">
+			<span className="header_logo-wrapper"><img src={logo} alt="logo" /></span>
+		</div>
+	)
+}
+
 
 export default App;
